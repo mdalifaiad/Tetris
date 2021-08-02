56fd1ee5871319faf3697d55e2fd9c4b722e04c5
Faiad <faiad@linkstaff.co.jp>
2021-08-02 18:15:02 +0900

    resize window handled

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,13 @@
 int main()
 {
     // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window", sf::Style::Titlebar | sf::Style::Close);
+    sf::RenderWindow window(sf::VideoMode(800.f, 600.f), "My window" /*, sf::Style::Titlebar | sf::Style::Close*/);
 
     sf::CircleShape circle;
-    circle.setPosition(25, 25);
-    circle.setOrigin(25, 25);
-    circle.setRadius(50);
-
+    circle.setPosition(25.f, 25.f);
+    circle.setOrigin(25.f, 25.f);
+    circle.setRadius(50.f);
     circle.setFillColor(sf::Color(255, 255, 77));
-    // set a 10-pixel wide orange outline
-    // circle.setOutlineThickness(10.f);
-    // circle.setOutlineColor(sf::Color(250, 150, 100));
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -28,11 +24,32 @@ int main()
             {
                 // window closed
                 window.close();
+            } // catch the resize events
+            else if (event.type == sf::Event::Resized)
+            {
+                // update the view to the new size of the window
+                sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+                window.setView(sf::View(visibleArea));
             }
             // key pressed
             else if (event.type == sf::Event::KeyPressed)
             { // move the entity relatively to its current position
-                circle.move(1.f, 5.f);
+                if (event.key.code == sf::Keyboard::Up)
+                {
+                    circle.move(0.f, -5.f);
+                }
+                if (event.key.code == sf::Keyboard::Down)
+                {
+                    circle.move(0.f, 5.f);
+                }
+                if (event.key.code == sf::Keyboard::Left)
+                {
+                    circle.move(-5.f, 0.f);
+                }
+                if (event.key.code == sf::Keyboard::Right)
+                {
+                    circle.move(5.f, 0.f);
+                }
             }
             // we don't process other types of events
             else
