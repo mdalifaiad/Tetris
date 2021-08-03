62a1f3638f745065eec4c540677762a81918572e
Faiad <faiad@linkstaff.co.jp>
2021-08-03 13:10:07 +0900

    window collission detection and velocity reverse

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 
 int main()
 {
     // create the window
-    sf::RenderWindow window(sf::VideoMode(800.f, 600.f), "My window" /*, sf::Style::Titlebar | sf::Style::Close*/);
+    unsigned WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "My window" /*, sf::Style::Titlebar | sf::Style::Close*/);
+    window.setFramerateLimit(240);
 
-    sf::CircleShape circle;
-    circle.setPosition(25.f, 25.f);
-    circle.setOrigin(25.f, 25.f);
-    circle.setRadius(50.f);
-    circle.setFillColor(sf::Color(255, 255, 77));
+    sf::Clock clock;
+    float dt, multiplier = 60.f;
+    sf::Vector2f velocity, v;
+    v.x = 8.f;
+    v.y = 8.f;
+
+    const float gridSize = 50.f;
+
+    sf::RectangleShape player;
+    // player.setPosition(100.f, 100.f);
+    // player.setOrigin(25.f, 25.f);
+    // player.setRadius(50.f);
+    player.setFillColor(sf::Color(255, 255, 77));
+    player.setSize(sf::Vector2f(gridSize, gridSize));
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -19,51 +32,84 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
+            // "close requested" event: we close the window OR Esc window close
+            if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
             {
                 // window closed
                 window.close();
-            } // catch the resize events
-            else if (event.type == sf::Event::Resized)
+            }
+
+            // catch the resize events
+            if (event.type == sf::Event::Resized)
             {
                 // update the view to the new size of the window
                 sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+                WINDOW_WIDTH = event.size.width;
+                WINDOW_HEIGHT = event.size.height;
                 window.setView(sf::View(visibleArea));
             }
-            // key pressed
-            else if (event.type == sf::Event::KeyPressed)
-            { // move the entity relatively to its current position
-                if (event.key.code == sf::Keyboard::Up)
-                {
-                    circle.move(0.f, -5.f);
-                }
-                if (event.key.code == sf::Keyboard::Down)
-                {
-                    circle.move(0.f, 5.f);
-                }
-                if (event.key.code == sf::Keyboard::Left)
-                {
-                    circle.move(-5.f, 0.f);
-                }
-                if (event.key.code == sf::Keyboard::Right)
-                {
-                    circle.move(5.f, 0.f);
-                }
-            }
-            // we don't process other types of events
-            else
-            {
+        }
 
-                break;
-            }
+        // frame update time
+        dt = clock.restart().asSeconds();
+
+        // Update
+
+        // velocity
+        // key pressed
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            v.x = 0.f;
+            v.y = -5.f;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            v.x = -5.f;
+            v.y = 0.f;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            v.x = 0.f;
+            v.y = 5.f;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            v.x = 5.f;
+            v.y = 0.f;
+        }
+
+        velocity.x = v.x * dt * multiplier;
+        velocity.y = v.y * dt * multiplier;
+
+        // auto update
+        player.move(velocity.x, velocity.y);
+
+        if (player.getPosition().x < 0)
+        {
+            player.setPosition(0, player.getPosition().y);
+            v.x *= -1;
+        }
+        if (player.getPosition().y < 0)
+        {
+            player.setPosition(player.getPosition().x, 0);
+            v.y *= -1;
+        }
+        if (player.getPosition().x + gridSize > WINDOW_WIDTH)
+        {
+            player.setPosition(WINDOW_WIDTH - gridSize, player.getPosition().y);
+            v.x *= -1;
+        }
+        if (player.getPosition().y + gridSize > WINDOW_HEIGHT)
+        {
+            player.setPosition(player.getPosition().x, WINDOW_HEIGHT - gridSize);
+            v.y *= -1;
         }
 
         // clear the window with black color
         window.clear(sf::Color::Black);
 
         // draw everything here...
-        window.draw(circle);
+        window.draw(player);
 
         // end the current frame
         window.display();
