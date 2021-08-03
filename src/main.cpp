9ead068637eea4188dbbcce1b195398a43b2d896
Faiad <faiad@linkstaff.co.jp>
2021-08-03 14:30:33 +0900

    texture files added

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
 
 int main()
 {
     // create the window
     unsigned WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "My window" /*, sf::Style::Titlebar | sf::Style::Close*/);
-    window.setFramerateLimit(240);
+    window.setFramerateLimit(144);
 
     sf::Clock clock;
     float dt, multiplier = 60.f;
-    sf::Vector2f velocity, v;
-    v.x = 8.f;
-    v.y = 8.f;
+    // sf::Vector2f velocity, v;
+    // v.x = 8.f;
+    // v.y = 8.f;
 
     const float gridSize = 50.f;
 
+    // player
     sf::RectangleShape player;
-    // player.setPosition(100.f, 100.f);
-    // player.setOrigin(25.f, 25.f);
+    player.setPosition(gridSize / 2, gridSize / 2);
+    player.setOrigin(gridSize / 2, gridSize / 2);
     // player.setRadius(50.f);
-    player.setFillColor(sf::Color(255, 255, 77));
+    // player.setFillColor(sf::Color(255, 255, 77));
     player.setSize(sf::Vector2f(gridSize, gridSize));
+
+    // player texture
+    sf::Texture playerTexture;
+    playerTexture.loadFromFile("assets/images/soilTexture.jpg");
+    player.setTexture(&playerTexture);
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -57,53 +61,53 @@ int main()
 
         // velocity
         // key pressed
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-            v.x = 0.f;
-            v.y = -5.f;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        {
-            v.x = -5.f;
-            v.y = 0.f;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        {
-            v.x = 0.f;
-            v.y = 5.f;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {
-            v.x = 5.f;
-            v.y = 0.f;
-        }
+        // if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        // {
+        //     v.x = 0.f;
+        //     v.y = -5.f;
+        // }
+        // if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        // {
+        //     v.x = -5.f;
+        //     v.y = 0.f;
+        // }
+        // if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        // {
+        //     v.x = 0.f;
+        //     v.y = 5.f;
+        // }
+        // if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        // {
+        //     v.x = 5.f;
+        //     v.y = 0.f;
+        // }
 
-        velocity.x = v.x * dt * multiplier;
-        velocity.y = v.y * dt * multiplier;
+        // velocity.x = v.x * dt * multiplier;
+        // velocity.y = v.y * dt * multiplier;
 
-        // auto update
-        player.move(velocity.x, velocity.y);
+        // // auto update
+        // player.move(velocity.x, velocity.y);
 
-        if (player.getPosition().x < 0)
-        {
-            player.setPosition(0, player.getPosition().y);
-            v.x *= -1;
-        }
-        if (player.getPosition().y < 0)
-        {
-            player.setPosition(player.getPosition().x, 0);
-            v.y *= -1;
-        }
-        if (player.getPosition().x + gridSize > WINDOW_WIDTH)
-        {
-            player.setPosition(WINDOW_WIDTH - gridSize, player.getPosition().y);
-            v.x *= -1;
-        }
-        if (player.getPosition().y + gridSize > WINDOW_HEIGHT)
-        {
-            player.setPosition(player.getPosition().x, WINDOW_HEIGHT - gridSize);
-            v.y *= -1;
-        }
+        // if (player.getPosition().x < 0)
+        // {
+        //     player.setPosition(0, player.getPosition().y);
+        //     v.x *= -1;
+        // }
+        // if (player.getPosition().y < 0)
+        // {
+        //     player.setPosition(player.getPosition().x, 0);
+        //     v.y *= -1;
+        // }
+        // if (player.getPosition().x + gridSize > WINDOW_WIDTH)
+        // {
+        //     player.setPosition(WINDOW_WIDTH - gridSize, player.getPosition().y);
+        //     v.x *= -1;
+        // }
+        // if (player.getPosition().y + gridSize > WINDOW_HEIGHT)
+        // {
+        //     player.setPosition(player.getPosition().x, WINDOW_HEIGHT - gridSize);
+        //     v.y *= -1;
+        // }
 
         // clear the window with black color
         window.clear(sf::Color::Black);
